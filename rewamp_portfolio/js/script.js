2a9912f2f83f072d0c43ddc12d8fb3310126a0f4
Trivikram244 <trivikram244@gmail.com>
2018-12-02 23:03:27 +0530

    Landing page
    Create
    .HTML
    .CSS
    .JS
    images

---
+++ rewamp_portfolio/js/script.js
@@ -0,0 +1,10 @@
+$(window).on("scroll", function() {
+    if($(window).scrollTop() > 50) {
+        $(".header").addClass("active");
+        $(".logo img").attr("src", "images/logo_black.png");
+    } else {
+        //remove the background property so it comes transparent again (defined in your css)
+       $(".header").removeClass("active");
+       $(".logo img").attr("src", "images/logo_white.png");
+    }
+});
