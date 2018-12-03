54ea477e3b1163bde41d0738cc2666be4b9d1ed5
Trivikram244 <trivikram244@gmail.com>
2018-12-03 17:34:43 +0530

    header, navigation bar fixed
    Nav added

--- rewamp_portfolio/js/script.js
+++ rewamp_portfolio/js/script.js
@@ -2,9 +2,30 @@ $(window).on("scroll", function() {
     if($(window).scrollTop() > 50) {
         $(".header").addClass("active");
         $(".logo img").attr("src", "images/logo_black.png");
+        $(".icon a").css("color", "black");
     } else {
         //remove the background property so it comes transparent again (defined in your css)
        $(".header").removeClass("active");
        $(".logo img").attr("src", "images/logo_white_text.png");
+       $(".icon a").css("color", "White");
+
     }
 });
+
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+    // lockScroll();
+}
+// function lockScroll() {
+//             if ($('body').hasClass('lock-scroll')) {
+//                 $('body').removeClass('lock-scroll');
+//             }
+//             else {
+//                 $('body').addClass('lock-scroll');
+//             }
+//       }
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
