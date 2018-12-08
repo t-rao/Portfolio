903d097834f6e4e2cdf6521fc1eb7ff925f91644
Trivikram244 <trivikram244@gmail.com>
2018-12-08 08:25:46 +0530

    Fixed issues
    Done

--- rewamp_portfolio/js/script.js
+++ rewamp_portfolio/js/script.js
@@ -1,23 +1,40 @@
 $(window).on("scroll", function() {
-    if($(window).scrollTop() > 50) {
-        $(".header").addClass("active");
-        $(".logo img").attr("src", "images/logo_black.png");
-        $(".icon a").css("color", "black");
-        $(".nav_mu a").css("color", "black");
-    } else {
-        //remove the background property so it comes transparent again (defined in your css)
-       $(".header").removeClass("active");
-       $(".logo img").attr("src", "images/logo_white_text.png");
-       $(".icon a").css("color", "White");
-       $(".nav_mu a").css("color", "White");
+  if ($(window).scrollTop() > 50) {
+    $(".header").addClass("active");
+    $(".logo img").attr("src", "images/logo_black.png");
+    $(".icon a").css("color", "black");
+    $(".nav_mu a").css("color", "black");
+  } else {
+    //remove the background property so it comes transparent again (defined in your css)
+    $(".header").removeClass("active");
+    $(".logo img").attr("src", "images/logo_white_text.png");
+    $(".icon a").css("color", "White");
+      $(".nav_mu a").css("color", "White");
 
+  }
+  function myFunction(x) {
+    if (x.matches) { // If media query matches
+      $(".nav_mu a").css("color", "White");
+    } else {
+      console.log("<500")
     }
+  }
+
+  var x = window.matchMedia("(max-width: 500px)")
+  myFunction(x) // Call listener function at run time
+  x.addListener(myFunction) // Attach listener function on state changes
+
+
+
 });
 
 
+
+
+
 function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-    // lockScroll();
+  document.getElementById("myNav").style.width = "100%";
+  // lockScroll();
 }
 // function lockScroll() {
 //             if ($('body').hasClass('lock-scroll')) {
@@ -29,5 +46,36 @@ function openNav() {
 //       }
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
+  document.getElementById("myNav").style.width = "0%";
 }
+
+
+
+
+// Smooth scroll
+
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
